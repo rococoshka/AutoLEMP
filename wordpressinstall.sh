c6306baecc34252ef726a6b9c66ef0fec891d2ed
root <root@ip-172-31-14-75.us-east-2.compute.internal>
2021-03-11 20:14:52 +0000

    Install wp and wordpress. Configuration database and wordpress.

--- wordpressinstall.sh
+++ wordpressinstall.sh
@@ -1,12 +1,24 @@
 #!/bin/sh
 
-dbuser="$1"
-dbpassword="$2"
+yourdomain="$1"
+dbuser="$2"
+dbpassword="$3"
 
-mariadbuser_conf(){
-	mysql_secure_installation
-	mysql -e "GRANT ALL ON *.* TO '$dbuser'@'localhost' IDENTIFIED BY 'dbpassword' WITH GRANT OPTION; FLUSH PRIVILEGES; SHOW ENGINES;"
+wpcli_install() {
+	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+	php wp-cli.phar --info
+	chmod +x wp-cli.phar
+	sudo mv wp-cli.phar /usr/local/bin/wp
 }
 
-mariadbuser_conf
+setup_wordpress(){
+#	mysql_secure_installation
+#	mysql -e "CREATE DATABASE wordpress; GRANT ALL ON wordpress.* TO '$dbuser'@'localhost' IDENTIFIED BY '$dbpassword'; FLUSH PRIVILEGES;"
+#	mkdir /var/www/wordpress
+#	wp core download --allow-root --path=/var/www/wordpress
+	wp core config --path=/var/www/wordpress --allow-root --dbname=wordpress --dbuser=$dbuser --dbpass=$dbpassword --dbhost=localhost --dbprefix=wp_
+}
+
+#wpcli_install
+setup_wordpress
 
