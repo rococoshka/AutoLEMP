5c21b8f46e59ea024362ec4a52f1028cfbdd585b
rococoshka <75371106+rococoshka@users.noreply.github.com>
2021-03-12 13:07:37 +0300

    Add setup-site

--- lemp_wp_install.sh
+++ lemp_wp_install.sh
@@ -104,6 +104,43 @@ dehydrate_update() {
 	chmod +x /etc/dehydrated/hook.sh
 }
 
+setup_site () {
+printf "server{
+	server_name $yourdomain www.$yourdomain;
+	location ^~ /.well-known/acme-challenge {
+		alias /var/lib/dehydrated/acme-challenges;
+	}
+	location / {
+		try_files \$uri \$uri/ /index.php\$is_args\$args;
+	}
+	listen 443 ssl;
+	ssl_certificate /var/lib/dehydrated/certs/$yourdomain/fullchain.pem;
+	ssl_certificate_key /var/lib/dehydrated/certs/$yourdomain/privkey.pem;
+	location = /favicon.ico { log_not_found off; access_log off; }
+	location =/robots.txt { log_not_found off; access_log off; allow all; }
+	location ~* \\.(css|gif|ico|jpeg|jpg|js|png)\$ {
+		expires max;
+		log_not_found off;
+		}
+        root /var/www/wordpress;
+        index index.php;
+        location ~ \\.php\$ {
+                include snippets/fastcgi-php.conf;
+                fastcgi_pass unix:/var/run/php/php7.2-fpm.sock;
+		fastcgi_intercept_errors on;
+        }
+        location ~ /\\.ht {
+                deny all;
+        }
+	}
+server{
+        listen 80;
+        server_name $yourdomain www.$yourdomain;
+	return 301 https://\$host\$request_uri;
+	}" > /etc/nginx/sites-enabled/$yourdomain.conf
+	nginx -s reload
+}
+
 wpcli_install() {
 	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 	php wp-cli.phar --info
@@ -124,6 +161,7 @@ setup_nginx_full() {
 	setup_dehydrated
 	setup_certificate_siteconf
 	dehydrate_update
+	setup_site
 }
 
 setup_wordpress() {
